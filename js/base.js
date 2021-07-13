bd0eb294f9fe90c0e47b1964354aaa369075a744
Black-J08 <nitin88mehta@gmail.com>
2021-07-14 01:29:49 +0530

    Fixed Navbar for smaller screen

--- js/base.js
+++ js/base.js
@@ -1,6 +1,44 @@
 var scrollTopButton = document.getElementById("scroll-top-button");
+let togClick = 0;
 
-$(window).on('scroll', function () {
+$('.navbar-toggler').on('click', function () {
+    togClick++;
+    if (togClick % 2 == 0) {
+        setTimeout(() => {
+            $('.navbar').css({
+                "background-color": "rgba(0, 0, 0, 0)",
+                "border-bottom": "none",
+                "box-shadow": "none",
+            });
+
+            $('.navbar .nav-link').css({
+                "color": "#ebebeb"
+            });
+            $(window).on('scroll', navScroll);
+            scrollTopButton.style.visibility = "visible";
+        }, 400);
+    } else {
+        $('.navbar').css({
+            "background-color": "rgb(33,33,33)",
+            "border-bottom": "solid black 1px",
+            "box-shadow": "6px 0px 6px 4px rgba(0, 0, 0, 0.6)",
+        }).addClass('sticky-top');
+        $(window).off("scroll", navScroll);
+        scrollTopButton.style.visibility = "hidden";
+    }
+
+});
+
+$(document).ready(function () {
+    $(window).on('scroll', navScroll);
+});
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function navScroll() {
     if (($(window).scrollTop() > $('.headImage').height() * 2 / 3) || ($(window).scrollTop() > $('.carousel').height() * 2 / 3)) {
 
         $('.navbar').css({
@@ -42,9 +80,4 @@ $(window).on('scroll', function () {
         });
 
     }
-});
-
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+};
